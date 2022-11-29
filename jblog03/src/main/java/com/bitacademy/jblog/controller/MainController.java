908270d42b6987ae1b2f687dd72f258fb24a2a00
suntaehun <sth7862@naver.com>
2022-11-29 15:11:36 +0900

    first commit

---
+++ jblog03/src/main/java/com/bitacademy/jblog/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.bitacademy.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "main/index";
+	}
+}
